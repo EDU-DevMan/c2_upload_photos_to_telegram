24144b3445eeba7532aa85086d01652e17338c39
ivan <podstolniyivan@gmail.com>
2025-10-10 16:13:33 +0300

    full upgraded fetch_nasa_images.py

--- fetch_nasa_images.py
+++ fetch_nasa_images.py
@@ -1,11 +1,73 @@
 import os
-import sys
+import requests
+from requests.exceptions import HTTPError
+from urllib.parse import urlparse, unquote
 from environs import Env
 
-env = Env()
-env.read_env()
 
-NASA_API = env('NASA_API')
+PATH_IMAGES = "images"
+URLS_SPACEX = "https://api.nasa.gov/planetary/apod"
+NUMBER_PHOTOS = 50
 
 
-print(NASA_API)
+def creates_folder(filename):
+    try:
+        return os.makedirs(filename, exist_ok=True)
+    except FileExistsError:
+        return filename
+
+
+def get_checked_path(url, api):
+    payload = {'count': NUMBER_PHOTOS, 'api_key': api}
+
+    try:
+        response = requests.get('{}'.format(url), params=payload)
+        response.raise_for_status()
+        return response
+
+    except HTTPError as e:
+        if e.response.status_code == 404:
+            print(f"❌ Страница не найдена: {url}")
+            print(f"Ошибка: {e}")
+            return None
+        elif e.response.status_code == 403:
+            print(f"🛑 Доступ запрещен, проверти API: {url}")
+            print(f"Ошибка: {e}")
+            return None            
+        else:
+            print(f"⚠️ Другая HTTP ошибка: {e}")
+            raise
+    except Exception as e:
+        print(f"🚫 Общая ошибка: {e}")
+        return None
+
+
+def downloads_image(path, img_num, img):
+    with open('{}/{}'.format(path,
+                             "{}".format(img_num)
+                             ), 'wb') as file:
+        file.write(img)
+
+
+def returns_file_extension(url):
+
+    return unquote(os.path.split(urlparse(url).path)[1])
+
+
+if __name__ == '__main__':
+    env = Env()
+    env.read_env()
+
+    nasa_api = env('NASA_API')
+
+    if not os.path.exists(PATH_IMAGES):
+        creates_folder(PATH_IMAGES)
+
+    if get_checked_path(URLS_SPACEX, nasa_api):
+        for url in get_checked_path(URLS_SPACEX, nasa_api).json():
+            img_name = returns_file_extension(url["url"])
+            downloads_image(
+                PATH_IMAGES, img_name, requests.get(url["url"]).content)
+
+
+
