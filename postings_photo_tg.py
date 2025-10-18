fceab93350963c3370a4ebb495476ad39b353982
ivan <podstolniyivan@gmail.com>
2025-10-18 15:55:19 +0300

    replace publish photo to Telegram channel.

--- postings_photo_tg.py
+++ postings_photo_tg.py
@@ -1,5 +1,14 @@
+import os
+import sys
+import random
+import time
 import telegram
+
 from environs import Env
+from launch_id_spacex import get_launch_id
+
+
+PUBLICATIONS_ALARM = 14400
 
 
 if __name__ == '__main__':
@@ -7,6 +16,16 @@ if __name__ == '__main__':
     env.read_env()
 
     bot = telegram.Bot(token=env('TELEGRAM_API'))
+    namespace = get_launch_id().parse_args(sys.argv[1:])
 
-    bot.send_document(chat_id=env('TELEGRAM_CHANNAL'),
-                      document=open('images/hoag_hubble_960.jpg', 'rb'))
+    while True:
+        for root, dirs, files in os.walk("images/"):
+            random.shuffle(files)
+            for image in files:
+                bot.send_document(chat_id=env('TELEGRAM_CHANNAL'),
+                                  document=open(
+                                      'images/{}'.format(image), 'rb'))
+                if namespace.launch is None:
+                    time.sleep(PUBLICATIONS_ALARM)
+                else:
+                    time.sleep(int(namespace.launch))
