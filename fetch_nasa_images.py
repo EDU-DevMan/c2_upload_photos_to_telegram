fcc671e4b5540f822a72542e659174cceab6cea9
ivan <podstolniyivan@gmail.com>
2025-10-18 16:24:25 +0300

    added parse_args in fetch_nasa_images

--- fetch_nasa_images.py
+++ fetch_nasa_images.py
@@ -1,13 +1,15 @@
 import requests
+import sys
 from environs import Env
 from saves_images_directory import saves_image
 from get_checked_path import checked_path
 from get_image_name import returns_file_extension
+from launch_id_spacex import get_launch_id
 
 
 PATH_IMAGES = "images"
 URL_NASA = "https://api.nasa.gov/planetary/apod"
-NUMBER_PHOTOS = 2
+NUMBER_PHOTOS = 40
 
 
 if __name__ == '__main__':
@@ -15,9 +17,17 @@ if __name__ == '__main__':
     env.read_env()
 
     nasa_api = env('NASA_API')
+    namespace = get_launch_id().parse_args(sys.argv[1:])
 
     if checked_path(URL_NASA, NUMBER_PHOTOS, nasa_api):
-        for url in checked_path(URL_NASA, NUMBER_PHOTOS, nasa_api).json():
-            img_name = returns_file_extension(url["url"])
-            saves_image(
-                PATH_IMAGES, img_name, requests.get(url["url"]).content)
+        if namespace.launch is None:
+            for url in checked_path(URL_NASA, NUMBER_PHOTOS, nasa_api).json():
+                img_name = returns_file_extension(url["url"])
+                saves_image(
+                    PATH_IMAGES, img_name, requests.get(url["url"]).content)
+        else:
+            for url in checked_path(URL_NASA,
+                                    int(namespace.launch), nasa_api).json():
+                img_name = returns_file_extension(url["url"])
+                saves_image(
+                    PATH_IMAGES, img_name, requests.get(url["url"]).content)
