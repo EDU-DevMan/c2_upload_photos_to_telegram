1c805954610b464be837352dae0b5d7cc657f3d1
ivan <podstolniyivan@gmail.com>
2025-10-23 16:07:55 +0300

    rename saves_image(,,image_url>image.url)

--- saves_images_directory.py
+++ saves_images_directory.py
@@ -2,9 +2,9 @@ import os
 from create_image_directory import makes_directory
 
 
-def saves_image(path_save, image_name, image_path):
+def saves_image(path_save, image_name, image_url):
     if not os.path.exists(path_save):
         makes_directory(path_save)
 
     with open('{}/{}'.format(path_save, image_name), 'wb') as file:
-        file.write(image_path)
+        file.write(image_url)
