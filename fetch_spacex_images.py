4806461cc5c41177cee99166526cbf48587f1fd0
ivan <podstolniyivan@gmail.com>
2025-11-10 14:13:05 +0300

    replaced fetch_spacex_images.py

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -2,7 +2,7 @@ import requests
 from saves_images_directory import saves_image
 from check_url import get_checked_url
 from fetch_image_name import get_file_extension
-from argument_parsing import get_input_argument
+from spacex_argument_parsing import get_launch_id
 
 IMAGES_PATH = "images"
 SPACEX_URL = "https://api.spacexdata.com/v5/launches"
@@ -10,27 +10,19 @@ SPACEX_URL = "https://api.spacexdata.com/v5/launches"
 
 if __name__ == '__main__':
 
-    launche_id = get_input_argument().parse_args()
+    launche = get_launch_id().parse_args()
+    url = get_checked_url('{}/{}'.format(SPACEX_URL, launche.launch_id))
 
-    if launche_id.input_argument is None:
-        response_urls = get_checked_url(SPACEX_URL)
-        if response_urls:
-            for image_url in response_urls.json()[::-1]:
+    if url:
+        if launche.launch_id:
+            for image_url in url.json()["links"]["flickr"]["original"]:
+                saves_image(IMAGES_PATH, get_file_extension(image_url),
+                            requests.get(image_url).content)
+        else:
+            for image_url in url.json()[::-1]:
                 original_image_url = image_url["links"]["flickr"]["original"]
                 if original_image_url:
                     for image in original_image_url:
-                        saves_image(
-                            IMAGES_PATH,
-                            get_file_extension(image),
-                            requests.get(image).content)
+                        saves_image(IMAGES_PATH, get_file_extension(image),
+                                    requests.get(image).content)
                     break
-
-    else:
-        response_urls = get_checked_url('{}/{}'.format(
-            SPACEX_URL, launche_id.input_argument))
-        if response_urls:
-            for image in response_urls.json()["links"]["flickr"]["original"]:
-                saves_image(
-                    IMAGES_PATH,
-                    get_file_extension(image),
-                    requests.get(image).content)
---
+++ spacex_argument_parsing.py
@@ -0,0 +1,15 @@
+import argparse
+
+
+def get_launch_id():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('launch_id', nargs='?', default='',
+                        help="""Скрипт fetch_spacex_images.py на вход может
+                        принять только один аргумент - id запуска, пример:
+                        fetch_spacex_images.py 61e048ffbe8d8b66799018d1 ,
+                        где 61e048ffbe8d8b66799018d1 - id запуска.
+                        Скрипты можно запускать без аргумента.
+                        Подробно читать в README.)
+                        """)
+
+    return parser
