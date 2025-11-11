867b02c6b4ef6f62a06291686ad1d139cd610910
ivan <podstolniyivan@gmail.com>
2025-11-11 14:05:04 +0300

    replaced fetch_image_name

--- fetch_image_name.py
+++ fetch_image_name.py
@@ -4,4 +4,8 @@ from urllib.parse import urlparse, unquote
 
 def get_file_extension(url):
 
-    return unquote(os.path.split(urlparse(url).path)[1])
+    parsed_url = urlparse(url).path
+    splited_url = os.path.split(parsed_url)
+    file_extension = unquote(splited_url[1])
+
+    return file_extension
